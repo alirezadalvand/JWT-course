09d3725606f6f8c4b2cc0c5c10dc206120dcb0d2
alirezadalvand <alireza.d1339@gmail.com>
2022-02-05 11:43:38 +0330

    db added

---
+++ db.js
@@ -0,0 +1,55 @@
+const users = [
+  {
+    id: 1,
+    email: "example@gmail.com",
+    password: "123456",
+  },
+  {
+    id: 2,
+    email: "alireza.d1339@gmail.com",
+    password: "q1w2e3r4t5",
+  },
+  {
+    id: 3,
+    email: "baharhekmatmehr@yahoo.com",
+    password: "84ol4ngh7",
+  },
+];
+
+const publicPost = [
+  {
+    title: "free article about development",
+    content:
+      "really!!!!  you think I have time to explain for u?   it's just fake data for test my code idiot",
+  },
+  {
+    title: "free article about development",
+    content:
+      "really!!!!  you think I have time to explain for u?   it's just fake data for test my code idiot",
+  },
+  {
+    title: "free article about development",
+    content:
+      "really!!!!  you think I have time to explain for u?   it's just fake data for test my code idiot",
+  },
+];
+
+const privatePost = [
+  {
+    title: "perfect article about development",
+    content:
+      "Oh yes!!!!  you are rich budy... but I'm smarter than u....  don't trust any one",
+  },
+  {
+    title: "perfect article about development",
+    content:
+      "Oh yes!!!!  you are rich budy... but I'm smarter than u....  don't trust any one",
+  },
+  {
+    title: "perfect article about development",
+    content:
+      "Oh yes!!!!  you are rich budy... but I'm smarter than u....  don't trust any one",
+  },
+];
+
+module.exports = { users, privatePost, publicPost };
